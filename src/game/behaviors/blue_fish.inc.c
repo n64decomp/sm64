3c9fac199e3d3fa7abaf0db931256b7f95d25833
MegaMech <MegaMech@users.noreply.github.com>
2020-02-18 23:09:17 -0700

    Began documentation, updated defines names

--- src/game/behaviors/blue_fish.inc.c
+++ src/game/behaviors/blue_fish.inc.c
@@ -1,57 +1,81 @@
-// blue_fish.c.inc
+/**
+ * @file blue_fish.inc.c
+ * Implements behaviour and spawning for blue_fish located in the aquarium outside of SA.
+ *
+ */
 
+/**
+ * 
+ * 
+ * 
+ */
 void bhv_blue_fish_loop(void) {
     f32 sp24;
     switch (o->oAction) {
         case 0:
             func_8029ED98(0, 1.0f);
+            
+            // If oTimer then assign random values to floats and rotate fish.
             if (o->oTimer == 0) {
-                o->oBlueFishUnk100 = RandomSign() << 11;
-                o->oBlueFishUnkF4 = RandomFloat() * 2;
-                o->oBlueFishUnkF8 = (s32)(RandomFloat() * 30) & 0xFE;
+                o->oBlueFishRandomAngle = RandomSign() << 11;
+                o->oBlueFishRandomVel = RandomFloat() * 2;
+                o->oBlueFishRandomTime = (s32)(RandomFloat() * 30) & 0xFE;
                 sp24 = RandomFloat() * 5;
-                if (sp24 < 2.0f)
+                if (sp24 < 2.0f) {
                     o->oAngleVelPitch = random_f32_around_zero(128);
-                else
+                } else {
                     o->oAngleVelPitch = 0;
+                }
             }
-            o->oForwardVel = o->oBlueFishUnkF4 + 3.0f;
-            if (o->oTimer >= o->oBlueFishUnkF8 + 60)
+            // Set forward velocity
+            o->oForwardVel = o->oBlueFishRandomVel + 3.0f;
+            if (o->oTimer >= o->oBlueFishRandomTime + 60) {
                 o->oAction++;
-            if (o->oTimer < (o->oBlueFishUnkF8 + 60) / 2)
+            }
+            if (o->oTimer < (o->oBlueFishRandomTime + 60) / 2) {
                 o->oFaceAnglePitch += o->oAngleVelPitch;
-            else
+            } else {
                 o->oFaceAnglePitch -= o->oAngleVelPitch;
+            }
             o->oVelY = -sins(o->oFaceAnglePitch) * o->oForwardVel;
             break;
         case 1:
             func_8029ED98(0, 2.0f);
-            o->oMoveAngleYaw = (s32)(o->oBlueFishUnk100 + o->oMoveAngleYaw);
-            if (o->oTimer == 15)
+            o->oMoveAngleYaw = (s32)(o->oBlueFishRandomAngle + o->oMoveAngleYaw);
+            if (o->oTimer == 15) {
                 o->oAction++;
+            }
             break;
         case 2:
             func_8029ED98(0, 1.0f);
-            if (o->oTimer >= o->oBlueFishUnkF8 + 60)
+            if (o->oTimer >= o->oBlueFishRandomTime + 60) {
                 o->oAction++;
-            if (o->oTimer < (o->oBlueFishUnkF8 + 60) / 2)
+            }
+            if (o->oTimer < (o->oBlueFishRandomTime + 60) / 2) {
                 o->oFaceAnglePitch -= o->oAngleVelPitch;
-            else
+            } else {
                 o->oFaceAnglePitch += o->oAngleVelPitch;
+            }
             break;
         case 3:
             func_8029ED98(0, 2.0f);
-            o->oMoveAngleYaw = (s32)(o->oBlueFishUnk100 + o->oMoveAngleYaw);
-            if (o->oTimer == 15)
+            o->oMoveAngleYaw = (s32)(o->oBlueFishRandomAngle + o->oMoveAngleYaw);
+            if (o->oTimer == 15) {
                 o->oAction = 0;
+            }
             break;
     }
     o->oVelY = -sins(o->oFaceAnglePitch) * o->oForwardVel;
     obj_move_using_fvel_and_gravity();
-    if (o->parentObj->oAction == 2)
+    if (o->parentObj->oAction == 2) {
         mark_object_for_deletion(o);
+    }
 }
 
+/*
+ * Spawns fish 
+ *
+ */
 void bhv_tank_fish_group_loop(void) {
     struct Object *fish;
     s32 i;
@@ -66,8 +90,9 @@ void bhv_tank_fish_group_loop(void) {
             }
             break;
         case 1:
-            if (gMarioCurrentRoom != 15 && gMarioCurrentRoom != 7)
+            if (gMarioCurrentRoom != 15 && gMarioCurrentRoom != 7) {
                 o->oAction++;
+            }
             break;
         case 2:
             o->oAction = 0;
