b660466cf95d296870fac1a096eb173d64209f26
MegaMech <MegaMech@users.noreply.github.com>
2020-02-20 13:11:54 -0700

    Update blue_fish.inc.c

--- src/game/behaviors/blue_fish.inc.c
+++ src/game/behaviors/blue_fish.inc.c
@@ -6,12 +6,12 @@
 /**
  * Determines fish movement.
  */
-void bhv_blue_fish_loop(void) {
+void bhv_blue_fish_movement_loop(void) {
     f32 randomSwitch;
     switch (o->oAction) {
         
         // Initial phase after spawning
-        case BLUE_FISH_SPAWN:
+        case BLUE_FISH_ACT_DIVE:
             func_8029ED98(0, 1.0f);
             
             // If oTimer equals zero, then assign random values to floats and rotate fish.
@@ -46,8 +46,8 @@ void bhv_blue_fish_loop(void) {
             o->oVelY = -sins(o->oFaceAnglePitch) * o->oForwardVel;
             break;
         
-        // Animates and adjusts fish yaw angle, then progresses oAction.
-        case BLUE_FISH_RANDOM_YAW:
+        // Animates and adjusts fish yaw angle.
+        case BLUE_FISH_ACT_TURN:
             func_8029ED98(0, 2.0f);
             o->oMoveAngleYaw = (s32)(o->oBlueFishRandomAngle + o->oMoveAngleYaw);
             if (o->oTimer == 15) {
@@ -56,10 +56,10 @@ void bhv_blue_fish_loop(void) {
             break;
             
         // Animates and adjusts pitch velocity.
-        case BLUE_FISH_RANDOM_PITCH:
+        case BLUE_FISH_ACT_ASCEND:
             func_8029ED98(0, 1.0f);
             
-            // Progresses oActioon after some time. 
+            // Progresses oAction to BLUE_FISH_ACT_TURN_BACK after elapsed time. 
             if (o->oTimer >= o->oBlueFishRandomTime + 60) {
                 o->oAction++;
             }
@@ -72,14 +72,14 @@ void bhv_blue_fish_loop(void) {
             }
             break;
             
-        // Animates and adjusts yaw angle
-        case BLUE_FISH_RANDOM_YAW_2:
+        // Animates and turns fish around
+        case BLUE_FISH_ACT_TURN_BACK:
             func_8029ED98(0, 2.0f);
             o->oMoveAngleYaw = (s32)(o->oBlueFishRandomAngle + o->oMoveAngleYaw);
             
-            // Sets oAction to the spawn phase if the timer equals fifteen.
+            // Sets oAction to the BLUE_FISH_ACT_DIVE if the timer equals fifteen.
             if (o->oTimer == 15) {
-                o->oAction = BLUE_FISH_SPAWN;
+                o->oAction = BLUE_FISH_ACT_DIVE;
             }
             break;
     }
@@ -98,11 +98,11 @@ void bhv_blue_fish_loop(void) {
  * Spawns fifteen fish if mario resides in room fifteen or seven.
  * It moves at random within 200.0f
  */
-void bhv_tank_fish_group_loop(void) {
+void bhv_blue_fish_spawn_loop(void) {
     struct Object *fish;
     s32 i;
     switch (o->oAction) {
-        case BLUE_FISH_SPAWN:
+        case BLUE_FISH_ACT_DIVE:
             if (gMarioCurrentRoom == 15 || gMarioCurrentRoom == 7) {
                 
                 // spawns fifteen fish and moves them within 200.0f
@@ -111,20 +111,20 @@ void bhv_tank_fish_group_loop(void) {
                     translate_object_xyz_random(fish, 200.0f);
                 }
                 
-                // Proceed to next oAction phase.
+                // Proceed to BLUE_FISH_ACT_TURN phase.
                 o->oAction++;
             }
             break;
             
         // Sets next oAction phase if mario is not in rooms fifteen and seven.
-        case BLUE_FISH_RANDOM_YAW:
+        case BLUE_FISH_ACT_TURN:
             if (gMarioCurrentRoom != 15 && gMarioCurrentRoom != 7) {
                 o->oAction++;
             }
             break;
             
         // Sets oAction to the spawn phase.
-        case BLUE_FISH_RANDOM_PITCH:
+        case BLUE_FISH_ACT_ASCEND:
             o->oAction = BLUE_FISH_SPAWN;
             break;
     }
