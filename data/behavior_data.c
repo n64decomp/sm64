560989015163064c55ca77aec1c7cc98a3d0e27d
MegaMech <MegaMech@users.noreply.github.com>
2020-02-20 17:05:59 -0700

    Update behavior_data.c

--- data/behavior_data.c
+++ data/behavior_data.c
@@ -2507,7 +2507,7 @@ const BehaviorScript bhvLargeFishGroup[] = {
     END_LOOP(),
 };
 
-const BehaviorScript bhvFishManagement[] = {
+const BehaviorScript bhvFish[] = {
     BEGIN(OBJ_LIST_DEFAULT),
     OR_INT(oFlags, (OBJ_FLAG_COMPUTE_ANGLE_TO_MARIO | OBJ_FLAG_COMPUTE_DIST_TO_MARIO | OBJ_FLAG_SET_FACE_YAW_TO_MOVE_YAW | OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE)),
     SET_HOME(),
