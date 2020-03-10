5ebf8bfb9fb9cc1de6a5b7cc83374e475cb0949b
Collin Ferguson <collinpferguson@gmail.com>
2020-03-09 19:16:53 -0500

    Added s prefix to variable names

--- src/game/area.c
+++ src/game/area.c
@@ -52,12 +52,12 @@ s16 gCurrLevelNum = 1;
 /* 
  * The following two tables are used in get_mario_spawn_type() to determine spawn type
  * from warp behavior.
- * When looping through warpBehaviorSpawnTable, if the behavior function in the table matches
- * the spawn behavior executed, the index of that behavior is used with spawnTypeFromWarpBhv
+ * When looping through sWarpBhvSpawnTable, if the behavior function in the table matches
+ * the spawn behavior executed, the index of that behavior is used with sSpawnTypeFromWarpBhv
 */
 
 // D_8032CE9C
-const BehaviorScript *warpBehaviorSpawnTable[] = {
+const BehaviorScript *sWarpBhvSpawnTable[] = {
     bhvDoorWarp, bhvStar,       bhvExitPodiumWarp, bhvWarp,
     bhvWarpPipe, bhvFadingWarp, bhvWarps60,        bhvWarps64,
     bhvWarps68,  bhvWarps6C,    bhvDeathWarp,      bhvWarps74,
@@ -66,7 +66,7 @@ const BehaviorScript *warpBehaviorSpawnTable[] = {
 };
 
 // D_8032CEEC
-u8 spawnTypeFromWarpBhv[] = {
+u8 sSpawnTypeFromWarpBhv[] = {
     MARIO_SPAWN_UNKNOWN_01, MARIO_SPAWN_UNKNOWN_02, MARIO_SPAWN_UNKNOWN_03, MARIO_SPAWN_UNKNOWN_03,
     MARIO_SPAWN_UNKNOWN_03, MARIO_SPAWN_UNKNOWN_04, MARIO_SPAWN_UNKNOWN_10, MARIO_SPAWN_UNKNOWN_12,
     MARIO_SPAWN_UNKNOWN_13, MARIO_SPAWN_UNKNOWN_14, MARIO_SPAWN_DEATH,      MARIO_SPAWN_UNKNOWN_16,
@@ -131,8 +131,8 @@ u32 get_mario_spawn_type(struct Object *o) {
     const BehaviorScript *behavior = virtual_to_segmented(0x13, o->behavior);
 
     for (i = 0; i < 20; i++) {
-        if (warpBehaviorSpawnTable[i] == behavior) {
-            return spawnTypeFromWarpBhv[i];
+        if (sWarpBhvSpawnTable[i] == behavior) {
+            return sSpawnTypeFromWarpBhv[i];
         }
     }
     return 0;
