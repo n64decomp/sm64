58f1fba887cc7c5c8cb3d3513d22d26fc444b26b
MGRich <rmgrichdeveloper@gmail.com>
2020-04-14 13:45:42 -0500

    Funny fix Lol

--- actors/burn_smoke/model.inc.c
+++ actors/burn_smoke/model.inc.c
@@ -2,10 +2,10 @@
 
 // 0x040217C0
 static const Vtx burn_smoke_seg4_vertex_040217C0[] = {
-    {{{   -50,    -50,      0}, 0, {     0,    992}, {0x14, 0x0a, 0x0a, 0xff}}},
-    {{{    50,    -50,      0}, 0, {   992,    992}, {0x14, 0x0a, 0x0a, 0xff}}},
-    {{{    50,     50,      0}, 0, {   992,      0}, {0x14, 0x0a, 0x0a, 0xff}}},
-    {{{   -50,     50,      0}, 0, {     0,      0}, {0x14, 0x0a, 0x0a, 0xff}}},
+    { { { -50, -50, 0 }, 0, { 0, 992 }, { 0x14, 0x0a, 0x0a, 0xff } } },
+    { { { 50, -50, 0 }, 0, { 992, 992 }, { 0x14, 0x0a, 0x0a, 0xff } } },
+    { { { 50, 50, 0 }, 0, { 992, 0 }, { 0x14, 0x0a, 0x0a, 0xff } } },
+    { { { -50, 50, 0 }, 0, { 0, 0 }, { 0x14, 0x0a, 0x0a, 0xff } } },
 };
 
 // //! Wrong texture format. Called as rgba16, which makes the burn smoke appear
@@ -28,7 +28,7 @@ const Gfx burn_smoke_seg4_dl_04022000[] = {
 // 0x04022028 - 0x04022048
 const Gfx burn_smoke_seg4_dl_04022028[] = {
     gsSPVertex(burn_smoke_seg4_vertex_040217C0, 4, 0),
-    gsSP2Triangles( 0,  1,  2, 0x0,  0,  2,  3, 0x0),
+    gsSP2Triangles(0, 1, 2, 0x0, 0, 2, 3, 0x0),
     gsSPEndDisplayList(),
 };
 
@@ -44,7 +44,8 @@ const Gfx burn_smoke_seg4_dl_04022048[] = {
 // 0x04022070 - 0x040220C8
 const Gfx burn_smoke_seg4_dl_04022070[] = {
     gsSPDisplayList(burn_smoke_seg4_dl_04022000),
-    gsDPLoadTextureBlock(burn_smoke_seg4_texture_04021800, G_IM_FMT_RGBA, G_IM_SIZ_16b, 32, 32, 0, G_TX_CLAMP, G_TX_CLAMP, 5, 5, G_TX_NOLOD, G_TX_NOLOD),
+    gsDPLoadTextureBlock(burn_smoke_seg4_texture_04021800, G_IM_FMT_IA, G_IM_SIZ_16b, 32, 32, 0,
+                         G_TX_CLAMP, G_TX_CLAMP, 5, 5, G_TX_NOLOD, G_TX_NOLOD),
     gsSPDisplayList(burn_smoke_seg4_dl_04022028),
     gsSPDisplayList(burn_smoke_seg4_dl_04022048),
     gsSPEndDisplayList(),
